565ffa4216b3ae79e32887dd4a2b5129fbd321ec
Erik Grassens <erik.grassens@monkeyproofsolutions.nl>
2017-03-06 11:38:23 +0100

    Add (empty) Simulink context menu

--- sl_customization.m
+++ sl_customization.m
@@ -5,9 +5,11 @@ function sl_customization(cm)
 % Register custom menu in the Simulink Editor's menu bar.
 
 cm.addCustomMenuFcn('Simulink:MenuBar', @getCustomSimulinkMenu);
+cm.addCustomMenuFcn('Simulink:ContextMenu', @getCustomSimulinkContext);
 
 end
 
+%% Menu Bar
 function schemaFcns = getCustomSimulinkMenu(callbackInfo) %#ok<INUSD>
 % Custom menu function: returns schema functions
 
@@ -29,6 +31,34 @@ schema.label            = 'MonkeyProof Utils';
 
 % Initialize Customizers to add
 customizers             = {utils.Customizer(), styleguide.Customizer()};
+
+% Generate childrenFcns for schema
+schema.childrenFcns     = Customizer.getCustomizeMethods(customizers);
+
+end
+
+%% Context Menu
+function schemaFcns = getCustomSimulinkContext(callbackInfo) %#ok<INUSD>
+% Custom context menu function: returns schema functions
+
+schemaFcns = {@customContext};
+
+end
+
+function schema = customContext(callbackInfo) %#ok<INUSD>
+% Schema function: defines the custom menu
+
+% Initialize schema
+schema                  = sl_container_schema();
+
+% Set menu tag
+schema.tag              = 'MonkeyProof:Context';
+
+% Set menu label
+schema.label            = 'MonkeyProof Utils';
+
+% Initialize Customizers to add
+customizers             = {};
 
 % Generate childrenFcns for schema
 schema.childrenFcns     = Customizer.getCustomizeMethods(customizers);
